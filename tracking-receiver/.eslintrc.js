833eaeb8f086d5da89f6ffcadfac94ebada6b562
André Treib <andre.treib@eldorado.org.br>
2021-05-22 19:05:19 -0300

    chore: init git on tracking-receiver

---
+++ tracking-receiver/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
