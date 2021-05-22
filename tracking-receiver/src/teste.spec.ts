eceac09c3910c4a343ba451b861ed6572ba5d85c
André Treib <andre.treib@eldorado.org.br>
2021-05-22 19:44:33 -0300

    check jest setup 2

--- tracking-receiver/src/teste.spec.ts
+++ tracking-receiver/src/teste.spec.ts
@@ -3,6 +3,6 @@ import { example } from './teste';
 describe('Example', () => {
   it('SHould example', () => {
     const response = example();
-    expect(response).toEqual('teste');
+    expect(response).toEqual('novo retorno');
   });
 });
